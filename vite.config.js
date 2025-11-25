ee85948fa03e9a8e8c21f3bbee70d6aa2326c44d
Rob <daniels.robert922@gmail.com>
2025-11-25 11:35:34 -0500

    website rewrite

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    root: '.',           // ← tells Vite the project root
-    publicDir: 'public', // ← tells Vite where index.html lives
-    build: {
-        outDir: 'dist'
-    },
     server: {
         port: 3000,
         host: true
