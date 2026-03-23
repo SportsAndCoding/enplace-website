7f602022f0f82cdbdf8ab90b30b37149007f0ba3
Rob <daniels.robert922@gmail.com>
2026-03-23 11:48:51 -0400

    www.proof troubleshooting

--- middleware.js
+++ middleware.js
@@ -2,15 +2,14 @@ export default function middleware(request) {
     const url = new URL(request.url);
 
     if (url.hostname === 'www.proof.en-place.ai') {
-        url.pathname = '/proof/index.html';
         return new Response(null, {
             headers: {
-                'x-middleware-rewrite': url.toString(),
+                'x-middleware-rewrite': url.origin + '/proof/index.html',
             },
         });
     }
 }
 
 export const config = {
-    matcher: '/((?!assets|_vercel|proof-logo\\.svg|proof-favicon\\.svg|enplace-favicon\\.svg|[^/]+\\.[^/]+$).*)',
+    matcher: '/',
 };
